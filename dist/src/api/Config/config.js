039dc66e5b38a3853525ed0bda6c88b7a9676439
simone <simonemartino05@gmail.com>
2025-02-06 23:00:42 +0100

    Rimosso dist dal tracking di Git

--- dist/src/api/Config/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import { fileURLToPath } from "url";
-import path from "path";
-dotenv.config();
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
-//# sourceMappingURL=config.js.map
